c9e6a0430beb77a6d7bbade42fed835c8871f02b
Benjamin Fernandez <fernandezb@gwu.edu>
2018-10-01 19:54:55 -0400

    restructured cam and main method imp

--- sel_screenshot.py
+++ sel_screenshot.py
@@ -1,23 +1,23 @@
+##### live webcam screenshot
+
 from selenium import webdriver
 # from selenium.webdriver.common.keys import Keys
 import cv2
 import time
 import os
 
+j_cam = "https://www.youtube.com/watch?v=nKMuBisZsZI"
+driver = webdriver.Firefox()
 
 if __name__ == "__main__":
 
-    j_cam = "https://www.youtube.com/watch?v=nKMuBisZsZI"
-    driver = webdriver.Firefox()
-    driver.get("https://www.youtube.com/watch?v=nKMuBisZsZI")
+    driver.get(j_cam)
     time.sleep(2) #time to wait for buffering logo to leave image
     driver.save_screenshot("j_cam.png")
     driver.close()
 
     im = cv2.imread("j_cam.png")
-
-    # hard crops for window frame
-    crop_im = im[100:460,30:780]
+    crop_im = im[100:460,30:780] # hard crops for window frame
     cv2.imwrite("j_cam_cropped.png",crop_im)
 
     os.remove("j_cam.png")
