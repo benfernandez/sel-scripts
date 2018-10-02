eaae1f4bb0dc5dba774a9f52d3fbff1667bda316
Benjamin Fernandez <fernandezb@gwu.edu>
2018-10-02 15:24:39 -0400

    added image and string formatting for output

--- sel_screenshot.py
+++ sel_screenshot.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 import cv2
 import time
+import datetime
 import os
 
 options = Options()
@@ -11,16 +12,21 @@ driver = webdriver.Firefox(options=options)
 
 j_cam = "https://www.youtube.com/watch?v=nKMuBisZsZI" # japanese crosswalk
 b_cam ="https://www.youtube.com/watch?v=2UIA8xOVcOs" # bear creek
+t_cam = "https://www.youtube.com/watch?v=ydZBBAPE_mw" # thailand cafe cam
 
 if __name__ == "__main__":
 
     driver.get(j_cam)
-    time.sleep(1) #time to wait for buffering logo to leave image
-    driver.save_screenshot("j_cam.png")
+    time.sleep(2) #time to wait for buffering logo to leave image
+    driver.save_screenshot("[placeholder]_cam_{0}_{1}_{2}.png".format(str(datetime.datetime.now().month),
+                                                                    str(datetime.datetime.now().day),
+                                                                    str(datetime.datetime.now().year)))
     driver.close()
 
-    im = cv2.imread("j_cam.png")
-    crop_im = im[80:520,50:880] # hard crops for window frame
-    cv2.imwrite("j_cam_cropped.png",crop_im)
-
-    os.remove("j_cam.png")
+    filelist = os.listdir(".")
+    for file in filelist:
+        if file.endswith(".png"):
+            im = cv2.imread(file)
+            crop_im = im[80:520,50:880] # hard crops for window frame
+            cv2.imwrite(file[:-4]+"_cropped.png",crop_im)
+            os.remove(file)
