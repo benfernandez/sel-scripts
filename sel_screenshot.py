4799a6b456781ff000ecf28afea66acdd5dd2786
Benjamin Fernandez <fernandezb@gwu.edu>
2018-10-02 15:02:44 -0400

    added options for headless browser as well as bear cam

--- sel_screenshot.py
+++ sel_screenshot.py
@@ -1,23 +1,26 @@
 ##### live webcam screenshot
-
 from selenium import webdriver
-# from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options
 import cv2
 import time
 import os
 
-j_cam = "https://www.youtube.com/watch?v=nKMuBisZsZI"
-driver = webdriver.Firefox()
+options = Options()
+options.add_argument("--headless")
+driver = webdriver.Firefox(options=options)
+
+j_cam = "https://www.youtube.com/watch?v=nKMuBisZsZI" # japanese crosswalk
+b_cam ="https://www.youtube.com/watch?v=2UIA8xOVcOs" # bear creek
 
 if __name__ == "__main__":
 
     driver.get(j_cam)
-    time.sleep(2) #time to wait for buffering logo to leave image
+    time.sleep(1) #time to wait for buffering logo to leave image
     driver.save_screenshot("j_cam.png")
     driver.close()
 
     im = cv2.imread("j_cam.png")
-    crop_im = im[100:460,30:780] # hard crops for window frame
+    crop_im = im[80:520,50:880] # hard crops for window frame
     cv2.imwrite("j_cam_cropped.png",crop_im)
 
     os.remove("j_cam.png")
